2033d5f223001ff24d0e4c32fbd5334403717b96
Matt Pignatore <mattpigs@gmail.com>
2019-08-20 12:42:22 -0400

    creating a GitHub Page for my previously built wedding website

---
+++ nav-script.js
@@ -0,0 +1,18 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //Click burger and have menu appear/disappear
+    burger.addEventListener('click',()=> {
+        nav.classList.toggle('nav-active');
+    });    
+
+    //Click links and have the menu disappear
+    nav.addEventListener('click',()=> {
+        nav.classList.toggle('nav-active');
+    });
+
+}
+
+navSlide();
