2702c27f530defddeb02a835b352f401f7c54634
Matt Pignatore <mattpigs@gmail.com>
2019-11-03 17:01:18 -0500

    fixed the hotels page layout as well as the moment.js script running on the class names

--- assets/js/moment-file.js
+++ assets/js/moment-file.js
@@ -1,19 +1,22 @@
-var cutoffDate = document.querySelector('.cutoff-date');
-var daysFromNow = document.querySelector('.days-from-now');
-var momentCutoff = moment('2020-04-21').format('MMMM Qo YYYY');
+var cutoffDate = document.getElementsByClassName('cutoff-date');
+var daysFromNow = document.getElementsByClassName('days-from-now');
+
+var momentCutoff = moment('2020-04-21').format('MMMM Qo');
 var fromNow = moment([2020, 04, 21]).fromNow();
 
 var hiltonCutoffDate = document.getElementById('hilton-cutoff-date');
 var hiltonDaysFromNow = document.getElementById('hilton-days-from-now');
-var hiltonMomentCutoff = moment('2020-05-15').format('MMMM Qo YYYY');
+
+var hiltonMomentCutoff = moment('2020-05-15').format('MMMM Qo');
 var hiltonFromNow = moment([2020, 05, 15]).fromNow();
 
+for (var i = 0; i < cutoffDate.length; i++) {
+    cutoffDate[i].innerHTML = momentCutoff;
+}
 
+for (var i = 0; i < daysFromNow.length; i++) {
+    daysFromNow[i].innerHTML = fromNow;
+}
 
-console.log(cutoffDate);
-console.log(fromNow);
-
-cutoffDate.innerHTML = momentCutoff;
-daysFromNow.innerHTML = fromNow;
 hiltonCutoffDate.innerHTML = hiltonMomentCutoff;
 hiltonDaysFromNow.innerHTML = hiltonFromNow;
