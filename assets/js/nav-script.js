568ed216450bbaa9a03fe96a78e194e470c5bd28
Matt Pignatore <mattpigs@gmail.com>
2020-01-06 18:32:40 -0500

    added the rsvp functionality and hopefully the hotels page will be corrected...

--- assets/js/nav-script.js
+++ assets/js/nav-script.js
@@ -4,24 +4,17 @@ const navSlide = () => {
     const navLinks = document.querySelectorAll('.nav-links li');
 
     //Click burger and have menu appear/disappear
-    burger.addEventListener('click',()=> {
-        nav.classList.toggle('nav-active');
-    });    
-
-    //Click links and have the menu disappear
-    nav.addEventListener('click',()=> {
+    burger.addEventListener('click',() => {
         nav.classList.toggle('nav-active');
     });
 
+    //Click links and have the menu disappear
+    nav.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+    });
 }
 
 window.onload = function() {
     navSlide();
     document.body.classList.add('fade');
 }
-
-
-
-    
-
-
